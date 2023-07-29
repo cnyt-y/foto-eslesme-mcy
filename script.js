93b65d68f1a1397d2b7422b9608e736418543dd2
cnyt-y <56654010+cnyt-y@users.noreply.github.com>
2023-07-29 22:46:48 +0300

    random js fonsiyon eklendi

--- script.js
+++ script.js
@@ -16,16 +16,82 @@ const kartTemplate = `
 `;
 
 let randomNum = function () {
-    let randomArray = [];
+    let randomArray = []
+    let numberArray = []
 
-    for (let i = 0; i < 8; i++) {
-        
-        let randomNumbers = Math.floor(Math.random() * 99);
-        randomArray.push(randomNumbers, randomNumbers);
-        if (randomArray.length > 6) break;
+    for (let i = 1; i <= 99; i++) {
+        numberArray.push(i)
+    }
+
+    for (let i = 0; i < 4; i++) {
+        let randomNumbers = Math.floor(Math.random() * 99)
+        let num = numberArray[randomNumbers]
+
+        randomArray.push(num, num)
+        num.splice(randomNumber, 1);
     }
     return randomArray
 };
+/*
+Görev 2: Bu numaraları 1-99 arası(1 ve 99 dahil) sayılardan rastgele 4 çift oluşturacak şekilde üreten bir fonksiyon yazarak, kod bloğundaki array değerini bu fonksiyondan dönen array ile değiştiren kodları yazın
+*/
+const fotoNumaralari = [10, 20, 30, 20, 10, 40, 40, 30];
+
+
+console.log(fotoNumaralari);
+
+for (fotoNumara of fotoNumaralari) {
+    const yenikart = document.createElement("div");
+    yenikart.innerHTML = kartTemplate;
+    yenikart.classList.add("kart");
+    yenikart.querySelector(".kart-arkayuz img").src = `https://lipsum.app/id/${fotoNumara}/100x100`;
+    document.querySelector("div#oyun-cerceve").append(yenikart);
+
+    //Her bir karta tıklandığında "kartTiklama" fonksiyonu çalışacak.
+    yenikart.addEventListener("click", kartTiklama);
+}
+
+function kartTiklama(olay) {
+    //Tıklanan kartı seçilen olarak değişkene atayalım
+    const secilenKart = olay.currentTarget;
+
+    //Tıklanan kart eslesti classına sahipse daha önce başka kartla eşleşmiş ve zaten açık durumda demektir, işlem yapmayacağız.
+    if (secilenKart.classList.contains("eslesti") === true) {
+        return;
+    }
+
+    //Tıklanan ve açılan karta tekrar tıklanırsa işlem yapmayacağız.
+    if (secilenKart.classList.contains("acik") === true) {
+        return;
+    }
+
+    //Peşpeşe kartlara tıklandığında 2'den fazla kart tıklanmasını engellememiz gerekiyor.
+    const tumAcikKartlar = document.querySelectorAll(".acik");
+    if (tumAcikKartlar.length === 2) {
+        return;
+    }
+
+    //Açık olan kart varsa seçelim.
+    const acikKart = document.querySelector(".acik");
+
+    //Hiç açık kart yoksa tıklanan karta açık class veriyoruz ve fonksiyondan çıkıyoruz.
+    if (acikKart === null) {
+        secilenKart.classList.add("acik");
+
+        setTimeout(
+            () => {
+                secilenKart.classList.remove("acik");
+            }, 1500
+        );
+        return;
+    }
+
+    //Daha önce bir açık kartımız varmış, son seçilen karta da açık class vererek tersini çevirelim.
+    secilenKart.classList.add("acik");
+
+    //Açık kartlara ait img etiketlerinin src görsel dosyaları eşleşiyor mu?
+    const acikKartImg = acikKart.querySelector(".kart-arkayuz img");
+    const secilenKartImg = secilenKart.querySelector(".kart-arkayuz img");
 
     if (acikKartImg.src === secilenKartImg.src) {
         //İki açık kart arasında eşleşme var.
